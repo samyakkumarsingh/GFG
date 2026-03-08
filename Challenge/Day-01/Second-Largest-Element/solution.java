adb805ef8e22231741ea64c8c5aec7ea6fb8b808
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-03-08 17:38:27 +0530

    Implement method to find second largest element

---
+++ Challenge/Day-01/Second-Largest-Element/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int num : arr){
+            if(num > largest){
+                secondLargest = largest;
+                largest = num;
+            } 
+            else if(num > secondLargest && num != largest){
+                secondLargest = num;
+            }
+        }
+
+        return secondLargest;
+    }
+}
