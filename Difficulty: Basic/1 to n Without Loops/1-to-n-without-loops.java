cbe701636632b464f915b9d66953e3ccfdba20df
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-07-31 11:51:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/1 to n Without Loops/1-to-n-without-loops.java
+++ Difficulty: Basic/1 to n Without Loops/1-to-n-without-loops.java
@@ -1,14 +1,14 @@
 class Solution {
     static void printTillN(int N) {
         // code here
-        solve(1,N);
+        solve(N);
         return;
         
     }
-    public static void solve(int i,int N){
-        if(i>N) return;
-        System.out.print(i+" ");
-        solve(i+1,N);
-        return;
+    public static void solve(int n){
+        if(n<1) return;
+        solve(n-1);
+       System.out.print(n+" ");
+        
     }
 }
