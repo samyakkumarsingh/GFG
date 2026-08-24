7055508b31beadc6ae331372e46ae0fcafcbe517
Samyak kumar singh <115411154+samyakkumarsingh@users.noreply.github.com>
2026-08-24 10:38:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,36 +1,35 @@
 class Solution {
-    
-    public boolean possible(int[] arr,int k,int mid){
-        int cows=1;
-        int n=arr.length;
-        int prev=arr[0];
-        for(int i=1;i<n;i++){
-            if(arr[i]-prev>=mid){
-                cows++;
-                prev=arr[i];
-            }
-        }
-        if(cows>=k) return true;
-        return false;
-    }
-    
-    public int aggressiveCows(int[] arr, int k) {
-        // code here
-        Arrays.sort(arr);
-        int n=arr.length;
-        int low=1;
-        int high=arr[n-1]-arr[0];
-        int ans=high;
-        
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(possible(arr,k,mid)==true){
-                ans=mid;
-                low=mid+1;
-            }
-            else high=mid-1;
-        }
-        return ans;
-        
-    }
+	public int aggressiveCows(int[] arr, int k) {
+		// code here
+		int n=arr.length;
+		Arrays.sort(arr);
+		int low = 1;
+		int high = arr[n-1]-arr[0];
+		int ans = 0;
+		while (low <= high) {
+			int mid = low + (high - low)/2;
+			if (isPossible(mid, arr, k)) {
+				ans = mid;
+				low = mid + 1;
+			}
+			else
+				high = mid - 1;
+		}
+		return ans;
+	}
+	
+	public boolean isPossible(int mid, int[] arr, int k) {
+		int n = arr.length;
+		int cows = 1;
+		int prev = arr[0];
+		for (int i = 1; i<n; i++) {
+			if (arr[i]-prev >= mid) {
+				cows++;
+				prev = arr[i];
+			}
+		}
+		
+		if (cows >= k)
+			return true; else return false;
+	}
 }
